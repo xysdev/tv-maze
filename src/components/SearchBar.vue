a05fb527b38da5b05b56b6427c22cd7a6cb8ae76
xysdev <geramian.erfan@outlook.com>
2024-05-31 17:46:27 +0200

    add fetch shows api

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,15 +1,30 @@
 <template>
   <label for="searchbar" class="search-container">
-      <img src="https://img.icons8.com/material-rounded/24/000000/search.png" alt="Search" />
-    <input id="searchbar" type="text" placeholder="Search..." />
+    <img src="https://img.icons8.com/material-rounded/24/000000/search.png" alt="Search" />
+    <input id="searchbar" type="text" placeholder="Search..." @input="search" />
   </label>
 </template>
 
 <script lang="ts">
+import { State } from '@/store';
 import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'SearchBar',
+  setup() {
+    const store = useStore<State>();
+    const fetchShow = async (searchQuery: string) => {
+      await store.dispatch('shows/fetchShows', searchQuery);
+    };
+    return { fetchShow };
+  },
+  methods: {
+    search(event: Event) {
+      const target = event.target as HTMLInputElement;
+      this.fetchShow(target.value);
+    },
+  },
 });
 </script>
 
--- src/store/modules/shows.ts
+++ src/store/modules/shows.ts
@@ -5,11 +5,13 @@ import { Show } from '@/interfaces/show';
 interface State {
   shows: Show[];
   showsByGenre: { [genre: string]: Show[] };
+  searchedResults: Show[];
 }
 
 const mainState: State = {
   shows: [],
   showsByGenre: {},
+  searchedResults: [],
 };
 
 const getters = {
@@ -28,6 +30,9 @@ const mutations = {
   setShowsByGenre(state: State, showsByGenre: { [genre: string]: Show[] }) {
     state.showsByGenre = showsByGenre;
   },
+  setSearchedResults(state: State, shows: Show[]) {
+    state.searchedResults = shows;
+  },
   categorizeShowsByGenre(state: State) {
     const categorizedShows = state.shows.reduce((acc, show) => {
       show.genres.forEach((genre) => {
@@ -44,15 +49,24 @@ const mutations = {
 };
 
 const actions = {
-  async fetchShows({ commit }: ActionContext<State, any>, page = 0): Promise<Show[]> {
+  async fetchShows({ commit }: ActionContext<State, any>, page = 0): Promise<void> {
     try {
       const response = await axios.get<Show[]>(`https://api.tvmaze.com/shows?page=${page}`);
       const shows = response.data;
       commit('setShows', shows);
       commit('categorizeShowsByGenre');
-      return shows;
     } catch (error) {
       console.error('Error fetching shows:', error);
+      throw error;
+    }
+  },
+  async fetchShowByName({ commit }: ActionContext<State, any>, name: string): Promise<void> {
+    try {
+      const response = await axios.get<Show>(`https://api.tvmaze.com/search/shows?q=${name}`);
+      const shows = response.data;
+      commit('setSearchedResults', [shows]);
+    } catch (error) {
+      console.error('Error fetching show by name:', error);
       throw error;
     }
   },
