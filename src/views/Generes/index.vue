84468521d920069f894659a48e17c9b2bae6aef8
xysdev <geramian.erfan@outlook.com>
2024-06-01 16:41:19 +0200

    add genere page

---
+++ src/views/Generes/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ShowsGrid :shows="showGeneres[genere]" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import ShowsGrid from '@/layout/ShowsGrid/index.vue';
+import { State } from '@/store';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'Generes',
+  components: {
+    ShowsGrid,
+  },
+  props: {
+    genere: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    debugger;
+    const store = useStore<State>();
+    const showGeneres = computed(() => store.getters['shows/getShowsByGenre']);
+    return {
+      showGeneres,
+    };
+  },
+});
+</script>
+<style scoped lang="scss"></style>
