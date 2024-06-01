fd150df507c10ea0e2b95b165e967a3c44677e2d
xysdev <geramian.erfan@outlook.com>
2024-06-01 16:33:32 +0200

    move the fetch shows to app file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,15 @@
 <template>
-  <Navbar />
   <HeroSection v-if="selectedShow" :show="selectedShow" />
   <div v-for="(title, key) in showGenre" :key="key">
     <GenereSelection :shows="showsByGenere[key]" :title="title" />
   </div>
-  <AppFooter />
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import { State } from '@/store'; // Adjust the path according to your folder structure
-import AppFooter from '@/components/AppFooter.vue';
 import HeroSection from '@/components/HeroSection.vue';
-import Navbar from '@/layout/Navbar/index.vue';
 import { Show } from '@/interfaces/show';
 import GenereSelection from '@/components/GenereSelection.vue';
 import showGenre from '@/utils/showGeneres';
@@ -21,9 +17,7 @@ import showGenre from '@/utils/showGeneres';
 export default defineComponent({
   name: 'HomeView',
   components: {
-    Navbar,
     HeroSection,
-    AppFooter,
     GenereSelection,
   },
   setup() {
@@ -34,21 +28,8 @@ export default defineComponent({
       () => shows.value[Math.floor(Math.random() * shows.value.length)],
     );
 
-    const loadShows = async () => {
-      try {
-        await store.dispatch('shows/fetchShows');
-      } catch (error) {
-        console.error('Failed to load shows:', error);
-      }
-    };
-
-    onMounted(() => {
-      loadShows();
-    });
-
     return {
       showsByGenere,
-      loadShows,
       showGenre,
       selectedShow,
     };
