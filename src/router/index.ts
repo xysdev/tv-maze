dc31b4acac874646fca815c32565ebdeba5fff96
xysdev <geramian.erfan@outlook.com>
2024-06-03 08:20:21 +0200

    add all show page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,18 @@ import Home from '@/views/Home/index.vue';
 import Generes from '@/views/Generes/index.vue';
 import Show from '@/views/Show/index.vue';
 import Search from '@/views/Search/index.vue';
+import AllShows from '@/views/AllShows/index.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/shows/all',
+    name: 'All Shows',
+    component: AllShows,
   },
   {
     path: '/generes/:genere',
